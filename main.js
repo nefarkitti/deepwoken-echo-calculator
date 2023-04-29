bdb554b92b206301168b20b95bf04e854234a27b
Kenzi <89108711+nefarkitti@users.noreply.github.com>
2023-04-29 17:03:14 +0100

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,22 @@ const PowerInpt = document.getElementById("powerinpt")
 let echoes = 0
 let additive = 0
 
-echoCount.innerText = echoes
+let calc = 0.75 * PowerInpt.value
+echoes = Math.ceil(calc)
+echoCount.innerText = parseInt(echoes) + additive
+
+let initCheckboxes = document.querySelectorAll("input[type='checkbox']");
+for (const checkbox of initCheckboxes) {
+    console.log(checkbox.value)
+    if (checkbox.checked) {
+        console.log("YES!")
+        additive += parseInt(checkbox.value)
+    } else {
+        additive -= parseInt(checkbox.value)
+    }
+    echoCount.innerText = parseInt(echoes) + additive
+}
+
 
 document.querySelector("#powerinpt").addEventListener("input", test);
 
